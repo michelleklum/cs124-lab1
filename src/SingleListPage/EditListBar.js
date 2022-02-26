3f9294cc70428b6e18aef495f9d18e7e76101e9f
mlum123 <michellelum123@gmail.com>
2022-02-25 20:27:38 -0800

    Change SingleListPage Menu Mode EditListBar wording

--- src/SingleListPage/EditListBar.js
+++ src/SingleListPage/EditListBar.js
@@ -3,11 +3,16 @@ import React, { Fragment } from "react";
 function EditListBar(props) {
   return (
     <Fragment>
-      <i className="fas fa-palette customize-list-icon"
-        onClick={() => props.onChangePage("EditListPage")}></i>
-      <h2 className="customize-list-desc"
-        onClick={() => props.onChangePage("EditListPage")}>
-        Customize list appearance</h2>
+      <i
+        className="fas fa-palette customize-list-icon"
+        onClick={() => props.onChangePage("EditListPage")}
+      ></i>
+      <h2
+        className="customize-list-desc"
+        onClick={() => props.onChangePage("EditListPage")}
+      >
+        Customize list
+      </h2>
     </Fragment>
   );
 }
