85739ed1cb4f4c48e35a9a6f18ff24a35ff35d7e
Zoe Tokheim <zoetokheim@gmail.com>
2022-03-01 22:26:23 -0800

    Changed icon to .ico file and removed unused files

--- src/Home/HomeTopBar.js
+++ src/Home/HomeTopBar.js
@@ -1,13 +1,12 @@
 import "./HomeTopBar.css";
 import HomeSearchButton from "./HomeSearchButton";
-import SettingsButton from "./SettingsButton";
 
 function HomeTopBar(props) {
   return (
     <div className="top-bar" id="home-top-bar">
       <div className="top-bar-content">
         <div className="logo">
-          <img src={require("./logo_task_monster.png")} width="210"
+          <img src={require("./header_task_monster.png")} width="210"
             height="55" alt="Task Monster" />
         </div>
         <div className="right-aligned">
--- src/Home/SettingsButton.js
+++
@@ -1,9 +0,0 @@
-import "./SettingsButton.css";
-
-function SettingsButton() {
-  return (
-    <i className="settings-button fas fa-cog fa-4x"></i>
-  );
-}
-
-export default SettingsButton;
